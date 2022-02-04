0673593753bb07f5498f42ab73b82bff1f8cfbf1
Lawrence Gimenez <lawgimenez@icloud.com>
2022-02-04 10:19:35 +0800

    Initial drawing page

---
+++ Lucas Learning/View Controllers/DrawingViewController.swift
@@ -0,0 +1,72 @@
+//
+//  DrawingViewController.swift
+//  Lucas Learning
+//
+//  Created by Lawrence Gimenez on 2/4/22.
+//
+
+import UIKit
+import CoreGraphics
+
+class DrawingViewController: UIViewController {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    private var lastPoint = CGPoint.zero
+    private var color = UIColor.black
+    private var brushWidth: CGFloat = 10.0
+    private var opacity: CGFloat = 1.0
+    private var isSwiped = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        isSwiped = false
+        lastPoint = touch.location(in: view)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        isSwiped = true
+        let currentPoint = touch.location(in: view)
+        drawLine(from: lastPoint, to: currentPoint)
+        lastPoint = currentPoint
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isSwiped {
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
+        tempImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        tempImageView.image = nil
+    }
+    
+    private func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        tempImageView.image?.draw(in: view.bounds)
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(color.cgColor)
+        context.strokePath()
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+}
